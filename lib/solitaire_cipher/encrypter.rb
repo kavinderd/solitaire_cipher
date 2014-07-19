bdbd6fdb36d631db394d61e99c1c89bba5920db8
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-07-19 10:38:03 -0700

    decrypt

--- lib/solitaire_cipher/encrypter.rb
+++ lib/solitaire_cipher/encrypter.rb
@@ -10,15 +10,23 @@ module Encrypter
   	keystream.each do |char|
   	  number_keystream << (char.ord - 64)
   	end
-  	number_string = merge_string_keystream_numbers(number_string, number_keystream)
+  	number_string = add_string_keystream_numbers(number_string, number_keystream)
   	build_encryption(number_string)
   end
 
   def decrypt(string)
-
+    start_deck
+    group = format_string(string)
+    keystream = keystream(group.join.length)
+    keystream = format_string(keystream.join(""))
+    number_string = string_to_number_array(group)
+    number_keystream = string_to_number_array(keystream)
+    number_string = subtract_string_keystream_numbers(number_string, number_keystream)
+    build_encryption(number_string)
   end
 
   private
+
 
   def format_string(string)
     string = string.upcase.gsub(/[^0-9A-Za-z]/, '').scan(/.{1,5}/m)
@@ -40,7 +48,7 @@ module Encrypter
   	result
   end
 
-  def merge_string_keystream_numbers(number_string, number_keystream)
+  def add_string_keystream_numbers(number_string, number_keystream)
   	number_string.each_with_index do |number,i| 
   	  keystream_int = number_keystream[i]
   	  result = number + keystream_int
@@ -48,6 +56,16 @@ module Encrypter
   	  number_string[i]  = result
   	end
   	number_string
+  end
+
+  def subtract_string_keystream_numbers(number_string, number_keystream)
+    number_string.each_with_index do |number,i| 
+      keystream_int = number_keystream[i]
+      number += 26 if number <= keystream_int
+      result = number - keystream_int
+      number_string[i]  = result
+    end
+    number_string
   end
 
   def build_encryption(array)
@@ -59,3 +77,30 @@ module Encrypter
   end
 
 end
+=begin
+-def decrypt(message)
+ -  # Step 1
+ -  key_stream = generate(message.size)
+ -  
+ -  # Step 2
+ -  values = message.split("").map { |letter| letter[0] - ?A + 1 }
+ -
+ -  # Step 3
+ -  key_values = key_stream.split("").map { |letter| letter[0] - ?A + 1 }
+ -  
+ -  # Step 4
+ -  values.each_with_index do |value, index|
+ -    next if value < 0
+ -    if value <= key_values[index]
+ -      values[index] = value + 26 - key_values[index]
+ -    else
+ -      values[index] = value - key_values[index]
+ -    end
+ -  end
+ -  
+ -  # Step 5
+ -  message = (values.map { |number| (number - 1 + ?A).chr }).join("")
+ -  
+ -  return message
+ -end
+=end
